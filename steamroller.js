72bc9ce5a33e7e9193c9be124dca38d28f42ddd5
Natalie Acevedo <natalieacevedo25@gmail.com>
2020-12-21 11:28:33 +0100

    trying it with reduce unsuccesful

--- steamroller.js
+++ steamroller.js
@@ -1,24 +1,14 @@
 function steamrollArray(arr) {
-    
-    let vacio = [];
-    for (let i = 0; i < arr.length; i++){
-        if (!Array.isArray(arr[i])) {
-            vacio.push(arr[i])
-        }
-        else {
+   
+   return arr.reduce((acc, el) => {
         
-            for (let j = 0; j < arr[i].length; j++){
-                vacio.push(arr[i][j]);
-
-                if (Array.isArray(arr[i][j])) {
-                    vacio.concat(arr[i][j]);
-                }
-            }
-           // vacio.concat(arr[i]);
-        }
-    }
-  
-   return vacio;
-  }
+       if (!Array.isArray(el)) {
+           acc.push(el)
+       }
+       else {
+           acc.concat(steamrollArray(el))
+       }
+    }, [])
+};
   
   console.log(steamrollArray([1, [2], [3, [[4]]]]));
