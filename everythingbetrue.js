fe693dacdb26ab9c37d232d9da0b5070c3cf7be7
Natalie Acevedo <natalieacevedo25@gmail.com>
2020-12-22 10:23:03 +0100

    con hasOwnProperty

--- everythingbetrue.js
+++ everythingbetrue.js
@@ -1,11 +1,12 @@
-function truthCheck(collection, pre) {
-    for (let a in collection) {
-        if (!collection[a][pre]) {
+
+  function truthCheck(collection, pre) {
+    for (let a of collection) {
+        if (!a.hasOwnProperty(pre) || !a[pre]) {
             return false;
         }
     }
     return true;
 
 };
-  
+
  console.log (truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
