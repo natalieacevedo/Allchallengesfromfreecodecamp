19d49f3f42467fc754e26a1b2b614f3718a07d30
Natalie Acevedo <natalieacevedo25@gmail.com>
2020-12-29 12:03:56 +0100

    solucion buena modificando variable

--- makeaperson.js
+++ makeaperson.js
@@ -1,41 +1,33 @@
 var Person = function(firstAndLast) {
-    this.firstAndLast = firstAndLast;
-    
-    this.getFullName = function () {
-      return this.firstAndLast;
-    };
-    
-    this.getFirstName = function () {
-        let first = this.firstAndLast.split(' ')[0];
-        return first;
-    };
-    
-    this.getLastName = function () {
-        let apellido = this.firstAndLast.split(' ')[1];
-        return apellido;
-    };
-
-    this.setFullName = function (firstAndLast) {
-         this.firstAndLast = firstAndLast;
-    };
-
-    this.setFirstName = function (first) {
-        this.firstAndLast = `${first} ${this.firstAndLast.split(' ')[1]}`; 
-    
-    };
-
-    this.setLastName = function (last) {
-        this.firstAndLast = `${this.firstAndLast.split(' ')[0]} ${last}`
-    }
-
-
-
+    let nombreCompleto = firstAndLast;
+      
+      this.getFullName = function () {
+        return nombreCompleto;
+      };
+      
+      this.getFirstName = function () {
+          let first = nombreCompleto.split(' ')[0];
+          return first;
+      };
+      
+      this.getLastName = function () {
+          let apellido = nombreCompleto.split(' ')[1];
+          return apellido;
+      };
+  
+      this.setFullName = function (firstAndLast) {
+           nombreCompleto = firstAndLast;
+      };
+  
+      this.setFirstName = function (first) {
+          nombreCompleto = `${first} ${nombreCompleto.split(' ')[1]}`; 
+      
+      };
+  
+      this.setLastName = function (last) {
+          nombreCompleto = `${nombreCompleto.split(' ')[0]} ${last}`
+      }
+  
+  
+  
   };
-  
-  var bob = new Person('Bob Ross');
-
-bob.setFullName('haskell curry');
-
-console.log(Object.keys(bob).length);
-  
-
