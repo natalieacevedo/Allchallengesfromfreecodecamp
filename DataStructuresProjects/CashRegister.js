84e8448e2b7b2ac386aea2c53e450501025eb11c
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-01-27 10:36:49 +0100

    ultimo commit del cash register

--- DataStructuresProjects/CashRegister.js
+++ DataStructuresProjects/CashRegister.js
@@ -17,7 +17,7 @@ function checkCashRegister(price, cash, cid) {
         ["ONE HUNDRED", 100]
     ].reverse();
 
-    let verdades = false;
+   
     let i;
     
     let fin = [];
@@ -26,6 +26,7 @@ function checkCashRegister(price, cash, cid) {
     let cambio = cash - price;
     console.log(cambio);
     let totalCid = cid.reduce((acc, el) => (acc + el[1]), 0);
+    let segundoValor;
 
     if (totalCid < cambio) {
         finalObj.status = "INSUFFICIENT_FUNDS";
@@ -41,29 +42,31 @@ function checkCashRegister(price, cash, cid) {
        
         for (i = 0; i < backwards.length; i++) {
             let restaBackwards = backwards[i][1];
-            let segundoValor = 0;
-            while (cambio >= restaBackwards && segundoValor < backwards[i][1]) {
+            segundoValor = 0;
+           
+            while (restaBackwards > 0 && cambio >= values[i][1]) //|| remaining > ca) 
+            {
                 cambio -= values[i][1];
-                segundoValor += values[i][1];
                 restaBackwards -= values[i][1];
-               
-            }
-            fin.push(segundoValor);
+                segundoValor += values[i][1];
+        
+                cambio = Math.round(cambio* 100) / 100;
+            } 
+                
+            if (segundoValor > 0) {
+                fin.push(segundoValor);
+            
         }
-    
+        
+        }
        
     }
-    return fin;
+    finalObj.status = "OPEN";
+    finalObj.change = fin;
+    return finalObj;
    
 }
 
-   
-    
-
-
-
-
-  
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
  
 //{status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.//5], ["DIME", 0.2], ["PENNY", 0.04]]}.
